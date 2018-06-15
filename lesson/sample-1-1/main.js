14353425017c7a134e8982f6ef48d7dfe8a28d97
ctyo <ctyo@live.jp>
2018-06-15 19:39:17 +0900

    sample1-1に図形を追加

--- lesson/sample-1-1/main.js
+++ lesson/sample-1-1/main.js
@@ -7,5 +7,12 @@ function initMap() {
     center: {lat: 35.172899, lng: 136.887531},
     zoom: 15 
     });
+
+    var circle_coordinate = {lat: 35.174044, lng: 136.892795};
+    var circle = new google.maps.Circle({
+      map: map,
+      center: circle_coordinate,
+      radius: 100
+    });
 }
 
